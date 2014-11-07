4cad491ea373956129e16c33ef0a7f7e495d724a
littleandroide <jfragoso.perez@gmail.com>
2014-11-07 23:13:27 +0100

    creating package + calling it from hello

--- samples/hello/hello.go
+++ samples/hello/hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"com.jfragosoperez/samples/newmath"
+
+) 
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world. Sqrt(3) = %v\n", newmath.Sqrt(3))
+
 }
---
+++ samples/newmath/newmath.go
@@ -0,0 +1,14 @@
+//trivial example of a go library
+package newmath
+
+//Aprox. to the root square of x
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+} 
+
+//to compile this package call -> go build YOUR_PROJECT_LOCATION/ or
+//go build if you are in the source directory
