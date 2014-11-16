41a496a2cc0c0391a3a6614e8bfb6b581f19554b
littleandroide <jfragoso.perez@gmail.com>
2014-11-16 17:31:27 +0100

    removing not needed break, t.Errorf will exit, so the rest of the test is not executed

--- projecteuler/euler_test.go
+++ projecteuler/euler_test.go
@@ -3,14 +3,13 @@ package projecteuler
 import "testing"
 
 func TestFindMultiples(t *testing.T) {
-	const inMultipleOf, inUpTo, upperBoundForcingEmptyList = 5, 50, 1
+	const inMultipleOf, inUpTo, upperBoundForcingEmptyList = 5, 52, 1
 	outMultiples := []int{5, 10, 15, 20, 25, 30, 35, 40, 45, 50}
 	result := FindMultiples(inMultipleOf, inUpTo)
 	//non multiple-list
 	for i := 0; i < len(result); i++ {
 		if result[i] != outMultiples[i] {
 			t.Errorf("Multiple list of %v upTo %v is %v", inMultipleOf, inUpTo, outMultiples)
-			break
 		}
 	}
 	//empty multiple list
