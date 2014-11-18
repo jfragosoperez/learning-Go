c9ee8bb4b0a54098f5103eea3d6ef05a252ded2e
littleandroide <jfragoso.perez@gmail.com>
2014-11-18 10:26:12 +0100

    fibo+++

--- projecteuler/fibonacci/fibonacci_test.go
+++ projecteuler/fibonacci/fibonacci_test.go
@@ -4,11 +4,13 @@ package fibonacci
 import "testing"
 
 func TestFibonacci(t *testing.T) {
-	testNthTermShouldBe(0, 0, t)
-	testNthTermShouldBe(1, 1, t)
-	testNthTermShouldBe(2, 1, t)
-	testNthTermShouldBe(3, 2, t)
-	testNthTermShouldBe(20, 6765, t)
+
+	inNthTerms := [11]uint32{0,1,2,3,20,22,24,26,30,35,40}
+	outNthTermsValues := [11]uint32{0,1,1,2,6765,17711,46368,121393,832040,9227465,102334155}
+
+	for i := 0; i < len(inNthTerms); i++ {
+		testNthTermShouldBe(inNthTerms[i], outNthTermsValues[i], t)
+	}		
 }
 
 func testNthTermShouldBe(nthTerm, expectedValue uint32, t *testing.T) {
